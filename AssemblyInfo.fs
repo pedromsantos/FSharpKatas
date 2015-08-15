b863bbb37d047cc5b45f429c07b10e92add5bacf
Pedro Santos <github@mariodelrio.com>
2015-08-15 18:56:35 +0100

    Added solution and project.

---
+++ AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace FsharpKatas
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FsharpKatas")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("BytesTeam")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("BytesTeam")>]
+[<assembly: AssemblyTrademark("BytesTeam")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
