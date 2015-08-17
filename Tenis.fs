7b4112c2de5db5587f61f69e5927beb7a53dc10d
Pedro Santos <pedro@codurance.com>
2015-08-17 17:34:35 +0100

    Started tenis kata

---
+++ Tenis.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpKatas
+
+    module Tenis =
+        type Points = | Zero = 0 | Fifteen = 15 | Thirty = 30 | Forty = 40 
+        type Player = {Name:string; Points:Points}
+        
+        let newPlayer playerName =
+           { Name = playerName; Points = Points.Zero} 
+
+        let winball player =
+            { Name = player.Name; Points = Points.Fifteen}
+
+    module TenisTests =
+        open NUnit.Framework
+        open Tenis
+
+        [<Test>]
+        let ``Should increase points for ball winner from Zero to Fifteen on first win``() =
+            let player = winball(newPlayer "Player1")
+            Assert.That(player.Points, Is.EqualTo(Points.Fifteen))
