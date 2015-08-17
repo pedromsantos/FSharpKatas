2ea9a4ae4dd22c13d2cfd4794754d76a6b1d888f
Pedro Santos <pedro@codurance.com>
2015-08-17 14:57:55 +0100

    Implemented fibonacci 0, 1, 2

--- Fibonacci .fs
+++ Fibonacci .fs
@@ -2,13 +2,17 @@
 
     module Fibonacci =
         let fibonacci number =
-           1
+            match number with
+            | 2 -> 1
+            | n -> n
 
     module FibonacciTests =
         open NUnit.Framework
         open Fibonacci
 
+        [<TestCase(0, 0)>]
         [<TestCase(1, 1)>]
+        [<TestCase(2, 1)>]
         let ``Should determine fibonacci number for number``(number:int, expectedFibonacciNumber:int) =
             let fibonacciNumber = fibonacci number 
             Assert.AreEqual(expectedFibonacciNumber, fibonacciNumber)
