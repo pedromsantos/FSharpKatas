21a574bbd7c2fb49181a1f302eebbeff65c2fce8
Pedro Santos <pedro@codurance.com>
2015-08-17 15:03:13 +0100

    Implemented fibonacci 3, and 4

--- Fibonacci .fs
+++ Fibonacci .fs
@@ -4,6 +4,8 @@
         let fibonacci number =
             match number with
             | 2 -> 1
+            | 3 -> 2
+            | 4 -> 3
             | n -> n
 
     module FibonacciTests =
@@ -13,6 +15,9 @@
         [<TestCase(0, 0)>]
         [<TestCase(1, 1)>]
         [<TestCase(2, 1)>]
+        [<TestCase(2, 1)>]
+        [<TestCase(3, 2)>]
+        [<TestCase(4, 3)>]
         let ``Should determine fibonacci number for number``(number:int, expectedFibonacciNumber:int) =
             let fibonacciNumber = fibonacci number 
             Assert.AreEqual(expectedFibonacciNumber, fibonacciNumber)
