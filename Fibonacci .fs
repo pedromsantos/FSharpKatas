d6efd73df3fbddc8318184b690927ba8562e81fc
Pedro Santos <pedro@codurance.com>
2015-08-17 15:09:17 +0100

    Implemented fibonacci 7

--- Fibonacci .fs
+++ Fibonacci .fs
@@ -5,6 +5,7 @@
             match number with
             | 5 -> 5
             | 6 -> 8
+            | 7 -> 13
             | n when n > 1 -> n - 1
             | n when n <= 1 -> n
 
@@ -20,6 +21,7 @@
         [<TestCase(4, 3)>]
         [<TestCase(5, 5)>]
         [<TestCase(6, 8)>]
+        [<TestCase(7, 13)>]
         let ``Should determine fibonacci number for number``(number:int, expectedFibonacciNumber:int) =
             let fibonacciNumber = fibonacci number 
             Assert.AreEqual(expectedFibonacciNumber, fibonacciNumber)
