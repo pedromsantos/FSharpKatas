6db1bd892714bd1f272bf46f9777702155dce5db
Pedro Santos <pedro@codurance.com>
2015-08-17 17:51:32 +0100

    Implemented win second ball for a player

--- Tenis.fs
+++ Tenis.fs
@@ -1,14 +1,18 @@
 ﻿namespace FSharpKatas
 
     module Tenis =
+        open System
+
         type Points = | Zero = 0 | Fifteen = 15 | Thirty = 30 | Forty = 40 
         type Player = {Name:string; Points:Points}
         
         let newPlayer playerName =
-           { Name = playerName; Points = Points.Zero} 
+           { Name = playerName; Points = Points.Zero }
 
         let winball player =
-            { Name = player.Name; Points = Points.Fifteen}
+            let allPoints = Enum.GetValues(typeof<Points>)
+            let nextPoint = allPoints |> Seq.cast<Points> |> Seq.skipWhile (fun p -> p <> player.Points) |> Seq.skip 1 |> Seq.head
+            { Name = player.Name; Points = nextPoint }
 
     module TenisTests =
         open NUnit.Framework
@@ -17,4 +21,10 @@
         [<Test>]
         let ``Should increase points for ball winner from Zero to Fifteen on first win``() =
             let player = winball(newPlayer "Player1")
-            Assert.That(player.Points, Is.EqualTo(Points.Fifteen))
+            Assert.That(player.Points, Is.EqualTo(Points.Fifteen)) 
+            
+        [<Test>]
+        let ``Should increase points for ball winner from Fifteen to Thirty on second win``() =
+            let player = winball(newPlayer "Player1")
+            let player = winball(player)
+            Assert.That(player.Points, Is.EqualTo(Points.Thirty))
