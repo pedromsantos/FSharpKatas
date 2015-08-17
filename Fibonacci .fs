ac7c4f7f1ec0f9d61621dcb91a4ea69cc3dcf199
Pedro Santos <pedro@codurance.com>
2015-08-17 14:48:31 +0100

    Start Fibonacci kata first test green.

---
+++ Fibonacci .fs
@@ -0,0 +1,14 @@
+﻿namespace FSharpKatas
+
+    module Fibonacci =
+        let fibonacci number =
+           1
+
+    module FibonacciTests =
+        open NUnit.Framework
+        open Fibonacci
+
+        [<TestCase(1, 1)>]
+        let ``Should determine fibonacci number for number``(number:int, expectedFibonacciNumber:int) =
+            let fibonacciNumber = fibonacci number 
+            Assert.AreEqual(expectedFibonacciNumber, fibonacciNumber)
