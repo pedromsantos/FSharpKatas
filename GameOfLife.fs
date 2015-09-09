c9fa68a0e8f9c60bf0a8d19be846e39ce9863e3d
Pedro Santos <pedro@codurance.com>
2015-09-09 14:00:30 +0100

    Refactored cell type to separate cell from neighbours

--- GameOfLife.fs
+++ GameOfLife.fs
@@ -1,14 +1,13 @@
 ﻿namespace FSharpKatas
 
     module GameOfLife =
-        type CellStatus = Alive | Dead 
+        type Cell = Alive | Dead 
         
-        type Cell = { Status:CellStatus; Neighbours:Cell list }
-
         type X = X of int
         type Y = Y of int
         type Coordinate = X*Y 
 
+        type Neighbours =  Cell seq 
         type Universe = Cell option[,]
 
         type Seed = Map<Coordinate, Cell>
@@ -32,27 +31,27 @@
             | Some x, Some y -> Some (xCoordinate.Value, yCoordinate.Value)
             | _, _ -> None 
 
-        let private countAliveNeighbours cell =
-            cell.Neighbours 
-            |> Seq.filter (fun n -> n.Status = Alive) 
+        let private countAliveNeighbours neighbours =
+            neighbours 
+            |> Seq.filter (fun n -> n = Alive) 
             |> Seq.length
 
-        let private isCellAlive cell =
-            let aliveNeighbours = countAliveNeighbours cell
-            match cell.Status with
+        let private isCellAlive cell neighbours =
+            let aliveNeighbours = countAliveNeighbours neighbours
+            match cell with
             | Alive -> aliveNeighbours >= 2 && aliveNeighbours < 4
             | Dead -> aliveNeighbours = 3 
 
-        let private nextGenerationCellStatus cell =
-            match isCellAlive cell with
+        let private nextGenerationCellStatus cell neighbours =
+            match isCellAlive cell neighbours with
             | true -> Alive
             | false -> Dead
 
         let tickCell (cell:Cell) =
-            {cell with Status = nextGenerationCellStatus cell}
+            nextGenerationCellStatus cell
 
         let tick (universe:Universe) :Universe =
-            universe |> Array2D.map (fun c -> if c.IsSome then Some(tickCell c.Value) else c) 
+            universe |> Array2D.map (fun c -> if c.IsSome then Some(tickCell c.Value []) else c) 
 
         let init (seed:Seed) : Universe =
             Array2D.init<Cell option> size size (fun x y -> 
@@ -67,40 +66,31 @@
 
         [<Test>]
         let ``A live cell with fewer than two live neighbours dies, as if caused by under population``() = 
-            let neighbour1 = { Status=Alive; Neighbours=[] }
-            let neighbour2 = { Status=Dead; Neighbours=[] }
-            let neighbour3 = { Status=Dead; Neighbours=[] }
-            let cell = { Status=Alive; Neighbours=[neighbour1; neighbour2; neighbour3] }
+            let cell = Alive 
+            let neighbours = [Alive; Dead; Dead]
 
-            (tickCell cell) |> should equal { Status=Dead; Neighbours=[neighbour1; neighbour2; neighbour3] }
+            neighbours |> tickCell cell |> should equal Dead
 
         [<Test>]
         let ``A live cell with more than three live neighbours dies, as if by overcrowding``() = 
-            let neighbour1 = { Status=Alive; Neighbours=[] }
-            let neighbour2 = { Status=Alive; Neighbours=[] }
-            let neighbour3 = { Status=Alive; Neighbours=[] }
-            let neighbour4 = { Status=Alive; Neighbours=[] }
-            let cell = { Status=Alive; Neighbours=[neighbour1; neighbour2; neighbour3; neighbour4] }
+            let cell = Alive
+            let neighbours = [Alive; Alive; Alive; Alive]
 
-            tickCell cell |> should equal { Status=Dead; Neighbours=[neighbour1; neighbour2; neighbour3; neighbour4] }
+            neighbours |> tickCell cell |> should equal Dead; 
 
         [<Test>]
         let ``A live cell with two or three live neighbour’s lives on to the next generation``() = 
-            let neighbour1 = { Status=Alive; Neighbours=[] }
-            let neighbour2 = { Status=Alive; Neighbours=[] }
-            let neighbour3 = { Status=Alive; Neighbours=[] }
-            let cell = { Status=Dead; Neighbours=[neighbour1; neighbour2; neighbour3] }
+            let cell = Alive
+            let neighbours = [Alive; Alive; Alive]
 
-            tickCell cell |> should equal { Status=Alive; Neighbours=[neighbour1; neighbour2; neighbour3] }
+            neighbours |> tickCell cell |> should equal Alive;
 
         [<Test>]
         let ``A dead cell with exactly three live neighbours becomes a live cell``() = 
-            let neighbour1 = { Status=Alive; Neighbours=[] }
-            let neighbour2 = { Status=Alive; Neighbours=[] }
-            let neighbour3 = { Status=Alive; Neighbours=[] }
-            let cell = { Status=Dead; Neighbours=[neighbour1; neighbour2; neighbour3]}
+            let cell = Dead
+            let neighbours = [Alive; Alive; Alive]
 
-            tickCell cell |> should equal { Status=Alive; Neighbours=[neighbour1; neighbour2; neighbour3] }
+            neighbours |> tickCell cell |> should equal Alive
 
         [<Test>]
         let ``The Universe is created with a specified size``() =
@@ -117,7 +107,7 @@
         [<Test>]
         let ``The Universe can be seeded``() =
             let coordinate:Coordinate = (createCoordinate 0 0).Value
-            let cell:Cell = { Status=Alive; Neighbours=[] }
+            let cell:Cell = Alive
             let seed:Seed = [coordinate, cell] |> Map.ofList
             let universe = init seed
 
@@ -126,10 +116,10 @@
         [<Test>]
         let ``A Universe with a single live cell will bring no cells alive for next generation``() =
             let coordinate:Coordinate = (createCoordinate 1 1).Value
-            let cell:Cell = { Status=Alive; Neighbours=[] }
+            let cell:Cell = Alive
             let seed:Seed = [coordinate, cell] |> Map.ofList
             let universe = init seed
 
             let newUniverse = tick universe
 
-            newUniverse.[1,1].Value |> should equal { Status=Dead; Neighbours=[] }
+            newUniverse.[1,1].Value |> should equal Dead
