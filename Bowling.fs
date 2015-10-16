3ccdc0ca063efad153bdbaac41958733f89adc1c
Pedro Santos <pedro@codurance.com>
2015-10-16 16:54:45 +0100

    Extract function to generate rolls of same value for multiple frames in tests

--- Bowling.fs
+++ Bowling.fs
@@ -9,14 +9,17 @@
         open FsUnit
         open Bowling 
 
+        let rollSame howMany whatValue =
+           [for a in 1..howMany -> whatValue] 
+
         [<Test>]
         let ``Score should be 0 if all rolls in all frames are 0``() =
-            let result = score [for a in 1..20 -> 0]
+            let result = score (rollSame 20 0)
             result |> should equal 0
 
         [<Test>]
         let ``Score should be 20 if all rolls in all frames are 1``() =
-            let result = score [for a in 1..20 -> 1]
+            let result = score (rollSame 20 1) 
             result |> should equal 20
 
-
+        
