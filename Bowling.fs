53e9ed74121a7add1a86fc9a7b2a357a53cd36d3
Pedro Santos <pedro@codurance.com>
2015-10-16 16:45:07 +0100

    Bowling kata first test

---
+++ Bowling.fs
@@ -0,0 +1,15 @@
+﻿namespace FSharpKatas
+
+    module Bowling = 
+        let score frames =
+            0
+
+    module BowlingTests =
+        open NUnit.Framework
+        open FsUnit
+        open Bowling 
+
+        [<Test>]
+        let ``Score shoud be 0 if all rools ian all frames are 0``() =
+            let result = score [for a in 1..20 -> 0]
+            result |> should equal 0
