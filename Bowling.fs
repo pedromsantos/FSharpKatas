0d49c447bfdd5dfef5960e56d4bb2458a1f0939c
Pedro Santos <github@mariodelrio.com>
2015-10-18 22:26:25 +0100

    Implement strike score and perfect game score

--- Bowling.fs
+++ Bowling.fs
@@ -1,23 +1,39 @@
 ﻿namespace FSharpKatas
 
     module Bowling = 
-        
-        let isSpare firstRoll secondRoll =
+
+        let private isStrike firstRoll =
+            firstRoll = 10
+
+        let private scoreStrike firstRoll firstbonusRoll secondBonusRoll =
+            firstRoll + firstbonusRoll + secondBonusRoll
+
+        let private isSpare firstRoll secondRoll =
             firstRoll + secondRoll = 10
 
-        let scoreSpare firstRoll secondRoll bonusRoll =
+        let private scoreSpare firstRoll secondRoll bonusRoll =
             firstRoll + secondRoll + bonusRoll
         
-        let scoreFrame firstRoll secondRoll bonusRolls =
-            match isSpare firstRoll secondRoll with
-            | true -> (scoreSpare firstRoll secondRoll (List.head bonusRolls))
-            | _ -> firstRoll + secondRoll 
+        let private scoreFrame firstRoll secondRoll bonusRolls =
+            if isStrike firstRoll && bonusRolls |> List.length >= 2 then
+               scoreStrike firstRoll (bonusRolls |> List.head) (bonusRolls |> List.tail |> List.head)
+            elif isSpare firstRoll secondRoll then
+                scoreSpare firstRoll secondRoll (bonusRolls |> List.head)
+            else
+                firstRoll + secondRoll 
 
-        let rec score frames =
+        let rec private score frames =
             match frames with
             | [] -> 0
             | r1::r2::rest -> (scoreFrame r1 r2 rest) + score rest
             | [r] -> r
+
+        let scoreGame frames =
+            let gameScore = score frames
+            if gameScore > 300 then
+                300
+            else
+                gameScore
 
     module BowlingTests =
         open NUnit.Framework
@@ -32,16 +48,27 @@
 
         [<Test>]
         let ``Score should be 0 if all rolls in all frames are 0``() =
-            let result = score (rollSame 20 0)
+            let result = scoreGame (rollSame 20 0)
             result |> should equal 0
 
         [<Test>]
         let ``Score should be 20 if all rolls in all frames are 1``() =
-            let result = score (rollSame 20 1) 
+            let result = scoreGame (rollSame 20 1) 
             result |> should equal 20
 
         [<Test>]
-        let ``Score should be 16 for frames: 5 5 3 and all zeros afterwards, (5 5) is a spare``() =
+        let ``Score should be 16 for example single spare in first frame``() =
             let frames = 3::(rollSame 17 0) |> List.append rollSpare
-            let result = score frames 
+            let result = scoreGame frames 
             result |> should equal 16 
+
+        [<Test>]
+        let ``Score should be 24 for example single strike in first frame``() =
+            let frames = [10;0;4;3] 
+            let result = scoreGame frames 
+            result |> should equal 24
+
+        [<Test>]
+        let ``Score should be 300 for perfect game``() =
+            let result = scoreGame (rollSame 22 10)  
+            result |> should equal 300
