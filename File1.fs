ff836055a080c25c73112a80bb469043060decbb
Pedro Santos <pedro@codurance.com>
2016-01-11 16:52:50 +0000

    Add prims to do slides code highlight

---
+++ File1.fs
@@ -0,0 +1,16 @@
+﻿module File1
+
+    let incr =
+        let mutable counter = 0
+        counter <- counter + 1
+        counter
+
+    let incr1 =
+        let counter = ref 0
+        fun () ->
+            counter := !counter + 1
+            !counter;;
+
+    let caller =
+        let counter = incr
+        counter
