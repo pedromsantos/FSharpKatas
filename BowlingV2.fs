487e8710cd3db527f4d8dc5e6f5ce239bb441964
Pedro Santos <github@mariodelrio.com>
2016-05-06 23:32:46 +0100

    Started Bowling Kata V2

---
+++ BowlingV2.fs
@@ -0,0 +1,14 @@
+﻿namespace BowlingV2.FSharpKatas
+
+    module Bowling = 
+        let scoreGame rols =
+            0
+
+    module BowlingTests =
+        open NUnit.Framework
+        open Swensen.Unquote
+        open Bowling
+
+        [<Test>]
+        let ``Score should be 0 for input "--" ``() =
+            test <@ scoreGame "--" = 0 @>
