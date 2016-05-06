8b7c8de4019b9754e7f8d02b4e6551ef6f6f97ae
Pedro Santos <github@mariodelrio.com>
2016-05-06 23:48:38 +0100

    Sums normal rolls

--- BowlingV2.fs
+++ BowlingV2.fs
@@ -1,8 +1,12 @@
 ﻿namespace BowlingV2.FSharpKatas
 
     module Bowling = 
-        let scoreGame rols =
-            0
+        open System
+        
+        let scoreGame (rolls:string) =
+            rolls.Replace('-', '0')
+            |> Seq.map (fun r -> Int32.Parse(r.ToString()))
+            |> Seq.sum
 
     module BowlingTests =
         open NUnit.Framework
@@ -12,3 +16,7 @@
         [<Test>]
         let ``Score should be 0 for input "--" ``() =
             test <@ scoreGame "--" = 0 @>
+        
+        [<Test>]
+        let ``Score should be 2 for input "11" ``() =
+            test <@ scoreGame "11" = 2 @>
