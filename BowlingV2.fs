11cc9eefa23d4eec707eae046ca5a6bd5d251cf2
Pedro Santos <github@mariodelrio.com>
2016-05-07 00:32:58 +0100

    Scores spares

--- BowlingV2.fs
+++ BowlingV2.fs
@@ -3,9 +3,25 @@
     module Bowling = 
         open System
         
+        let parse roll =
+            match roll with
+            | '-' -> 0
+            | '/' -> 10
+            | r -> Int32.Parse(r.ToString())
+        
+        let scoreRoll index roll (rolls:int list) =
+            match roll with
+                | 10 -> 10 - rolls.[index - 1] + rolls.[index + 1]
+                | _ -> roll
+        
+        let scoreRolls (rolls:int list) =
+            rolls |> Seq.mapi (fun index roll -> scoreRoll index roll rolls) 
+        
         let scoreGame (rolls:string) =
-            rolls.Replace('-', '0')
-            |> Seq.map (fun r -> Int32.Parse(r.ToString()))
+            rolls
+            |> Seq.map parse
+            |> List.ofSeq
+            |> scoreRolls
             |> Seq.sum
 
     module BowlingTests =
@@ -20,3 +36,7 @@
         [<Test>]
         let ``Score should be 2 for input "11" ``() =
             test <@ scoreGame "11" = 2 @>
+            
+        [<Test>]
+        let ``Score should be 18 for input "5/4" ``() =
+            test <@ scoreGame "5/4" = 18 @>
