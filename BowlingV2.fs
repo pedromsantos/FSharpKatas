f3f872c2954ce00e7294d0e9356e4347bf97247f
Pedro Santos <github@mariodelrio.com>
2016-05-13 23:30:13 +0100

    Add Pins type

--- BowlingV2.fs
+++ BowlingV2.fs
@@ -4,21 +4,28 @@
         open System
 
         type RollType = Strike | Spare | Ball
-        type Roll = RollType * int
-
+        type Pins = Pins of int
+        type Roll = RollType * Pins
+        
         let maxBalls = 20
+        
+        let pinsDown roll =
+            let (Pins pins) = snd roll
+            pins
 
-        let parse roll index (rolls:String) =
-            let previousBall = fun () -> rolls |> Seq.item (index - 1)
+        let parse roll index rolls =
+            let previousBall = fun () -> Seq.item (index - 1) rolls
             match roll with
-            | '-' -> Ball, 0
-            | '/' -> Spare, 10 - Int32.Parse(previousBall().ToString())
-            | 'X' -> Strike, 10
-            | r -> Ball, Int32.Parse(r.ToString())
+            | '-' -> Ball, Pins 0
+            | '/' -> Spare, Pins (10 - Int32.Parse(previousBall().ToString()))
+            | 'X' -> Strike, Pins 10
+            | r -> Ball, Pins (Int32.Parse(r.ToString()))
 
         let scoreRoll index rolls =
             let rollValueForIndexPlus pad = 
-                if index + pad < Seq.length rolls then snd (Seq.item (index + pad) rolls) else 0        
+                if index + pad < Seq.length rolls 
+                then pinsDown (Seq.item (index + pad) rolls) 
+                else 0        
 
             let firstBonusBall = fun () -> rollValueForIndexPlus 1
             let secondBonusBall = fun () -> rollValueForIndexPlus 2
@@ -31,9 +38,9 @@
 
             match Seq.item index rolls with
                 | (_, _) when exceedsMaxBalls() -> 0
-                | (Spare, value) -> value + firstBonusBall()
-                | (Strike, value) -> value + firstBonusBall() + secondBonusBall()
-                | (Ball, value) -> value
+                | (Spare, Pins pins) -> pins + firstBonusBall()
+                | (Strike, Pins pins) -> pins + firstBonusBall() + secondBonusBall()
+                | (Ball, Pins pins) -> pins
 
         let scoreGame rolls =
             let parsedRolls = rolls |> Seq.mapi (fun index roll -> parse roll index rolls)
