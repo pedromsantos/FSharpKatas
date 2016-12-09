2dcbdd4080dcaa7d0b720b3a4aa3f63a9e2ef957
Pedro Santos <pedro@codurance.com>
2016-12-09 17:07:20 +0000

    Implement property base tests in leap year

--- LeapYears.fs
+++ LeapYears.fs
@@ -21,3 +21,42 @@
         [<TestCase(2000, true)>]
         let ``Should determine if year is a leap year`` year shouldBeLeapYear =
             test <@ leapYear year = shouldBeLeapYear @>
+
+    module LeapYearPropertyTests =
+        open FsCheck
+        open FsCheck.NUnit
+        open NUnit.Framework
+        open Swensen.Unquote
+        open LeapYear
+
+        let years = gen { 
+            let! i = Gen.choose (0, 2100) 
+            return i 
+        }
+
+        let isMultipleOfFour number =
+            number % 4 = 0
+
+        let isNotMultipleOfOneHundred number =
+            number % 100 <> 0
+
+        let isMultipleOfFourHundred number =
+            number % 400 = 0
+
+        let multiplesOfFourButNoOuneHundred = gen { 
+            return! (years |> Gen.suchThat (fun y -> isMultipleOfFour y && (isNotMultipleOfOneHundred y)))
+        }
+
+        let multiplesOfFourAndFourHundred = gen { 
+            return! (years |> Gen.suchThat (fun y -> isMultipleOfFour y && (isMultipleOfFourHundred y)))
+        }
+
+        [<Test>]
+        let ``Should be a leap year when divisible by 4 but not by 100``() =
+            Prop.forAll (Arb.fromGen multiplesOfFourButNoOuneHundred) (fun year -> LeapYear.leapYear year = true)
+            |> Check.VerboseThrowOnFailure
+
+        [<Test>]
+        let ``Should be a leap year when divisible by 4 and by 400``() =
+            Prop.forAll (Arb.fromGen multiplesOfFourAndFourHundred) (fun year -> LeapYear.leapYear year = true)
+            |> Check.VerboseThrowOnFailure
