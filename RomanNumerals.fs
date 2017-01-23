7fcc82a1277798c531fd92c2cb86bd0b7d69411a
Pedro Santos <pedro@codurance.com>
2017-01-23 17:10:52 +0000

    Format RomanNumerals

--- RomanNumerals.fs
+++ RomanNumerals.fs
@@ -2,19 +2,9 @@ namespace RomanNumerals.FSharpKatas
 
     module RomanNumerals =
         
-        let numbersToRomans = [1, "I"; 
-                               4, "IV"; 
-                               5, "V"; 
-                               9, "IX"; 
-                               10, "X"; 
-                               10, "X"; 
-                               40, "XL"; 
-                               50, "L"; 
-                               90, "XC"; 
-                               100, "C"; 
-                               400, "CD"; 
-                               500, "D"; 
-                               900, "CM"; 
+        let numbersToRomans = [1, "I"; 4, "IV"; 5, "V"; 9, "IX"; 
+                               10, "X"; 40, "XL"; 50, "L"; 90, "XC"; 
+                               100, "C"; 400, "CD"; 500, "D"; 900, "CM"; 
                                1000, "M"
                               ] |> List.rev
 
